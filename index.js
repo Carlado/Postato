bb6a5192fe1f72f4f87994941fdd99971336960f
Mikael Adolfsson <mikael_adolfsson@live.se>
2020-06-02 20:40:26 +0200

    Setup server route for billing

--- index.js
+++ index.js
@@ -10,6 +10,7 @@ require('./services/passport');
 mongoose.connect(keys.mongoURI);
 
 const app = express();
+app.use(express.json());
 
 app.use(
 	cookieSession({
@@ -22,6 +23,7 @@ app.use(passport.initialize());
 app.use(passport.session());
 
 require('./routes/authRoutes')(app);
+require('./routes/billingRoutes')(app);
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT);
