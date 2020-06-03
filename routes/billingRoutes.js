e373df15771ee5bfeea4a2ecf6b2d583b0dfbf16
Mikael Adolfsson <mikael_adolfsson@live.se>
2020-06-03 23:31:33 +0200

    Add stripe charge

--- routes/billingRoutes.js
+++ routes/billingRoutes.js
@@ -2,5 +2,14 @@ const keys = require('../config/keys');
 const stripe = require('stripe')(keys.stripeSecretKey);
 
 module.exports = (app) => {
-	app.post('/api/stripe', (req, res) => {});
+	app.post('/api/stripe', async (req, res) => {
+		const charge = await stripe.charges.create({
+			amount: 500,
+			currency: 'usd',
+			description: 'postato tokens',
+			source: req.body.id,
+		});
+
+		console.log(charge);
+	});
 };
